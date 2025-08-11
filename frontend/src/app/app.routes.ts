d88b494a935ecaa420700a3f04e0e245c79bae1b
Franco_SJ <jean.jara.fs@gmail.com>
2025-08-10 22:34:05 -0400

    feat(frontend): initial commit de frontend, con la configuracion de tailwind hecha

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { Login } from './core/features/auth/components/login/login';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: Login,
+  },
+];
